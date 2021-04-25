f3810dfe554a5299f91782b187e027b56d380260
Felipe Santana <felipepipa16@hotmail.com>
2021-04-25 13:29:23 -0300

    Migration to Supplies, supply schema and supply/restaurant relation created

--- lib/inmana/supply.ex
+++ lib/inmana/supply.ex
@@ -1,28 +1,28 @@
-defmodule Inmana.Restaurant do
+defmodule Inmana.Supply do
   # Aqui nós importamos o schema e o changeset. O schema é como se fosse uma struct
   # mas é utilizado pra BD
   # Já o changeset é um agrupamento de dados a serem mandados para o BD
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Inmana.Supply
-
+  alias Inmana.Restaurant
   # Configuramos a primary_key pra ter um id binario e ser gerado automaticamente
   @primary_key {:id, :binary_id, autogenerate: true}
 
   # Criamos um tipo de variavel geral do modulo
-  @required_params [:email, :name]
+  @required_params [:description, :expiration_date, :responsible, :restaurant_id]
 
   # Esse @derive diz ao Json Encoder como quais campos e como codificalos para Json
   @derive {Jason.Encoder, only: @required_params ++ [:id]}
 
   # Aqui definimos a tabela do nosso restaurante com os devidos campos
-  schema "restaurants" do
-    field :email, :string
-    field :name, :string
+  schema "supplies" do
+    field :description, :string
+    field :expiration_date, :date
+    field :responsible, :string
 
-    # Criamos a relacao de restaurant com supply
-    has_many :supplies, Supply
+    # Definimos aqui o relacionamento de supply com restaurat
+    belongs_to :restaurant, Restaurant
 
     # Pelo que entendi, essa função já traz alguns dados como "quando foi modificado"
     timestamps()
@@ -36,11 +36,9 @@ defmodule Inmana.Restaurant do
     |> cast(params, @required_params)
     # Dizemos que esses dados são obrigatórios
     |> validate_required(@required_params)
-    # Dizemos que o email tem que ter no mínimo dois caracteres
-    |> validate_length(:name, min: 2)
-    # Dizemos que o email deve ter o @, ~r serve para definir um regex
-    |> validate_format(:email, ~r/@/)
-    # Dizemos que o email deve ser único
-    |> unique_constraint([:email])
+    # Dizemos que a description tem que ter no mínimo dois caracteres
+    |> validate_length(:description, min: 3)
+    # Dizemos que o responsavel tem que ter no mínimo dois caracteres
+    |> validate_length(:responsible, min: 3)
   end
 end
